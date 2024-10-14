d6a308a75f2d98d1a129995c3dec608617c3bb1e
Saurav-9852 <saurav68265@gmail.com>
2024-10-15 01:03:26 +0530

    Initial commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const connect = mongoose.connect("mongodb://localhost:27017/Login-tut");
+const connect = mongoose.connect("mongodb+srv://prakashsaurav932004:Saurav123@cluster0.yn27c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 // check database connected or not
 connect.then(()=>{
@@ -29,4 +29,5 @@ const LoginSchema = new mongoose.Schema({
 const collection = new mongoose.model("users", LoginSchema);
 
 module.exports = collection;
+
 
